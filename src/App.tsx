3366bc1c41e00f96f14518911fa27a00030aacb3
Gbot-Prod <gilbert.dolz24@gmail.com>
2026-02-26 05:52:48 +0800

    I have no fucking clue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header/Header'
 import Timer from './components/Timer/Timer'
 import Stopwatch from './components/Stopwatch/Stopwatch'
 import PomodoroTimer from './components/PomodoroTimer/PomodoroTimer'
-import TicTacToe from './components/TicTacToe/TicTacToe'
 import ToDoList from './components/ToDoList/ToDoList'
 
 
